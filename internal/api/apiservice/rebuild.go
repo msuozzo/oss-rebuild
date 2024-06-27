743593d818be025e9de9c4b6a01d70d750d4657d
Matthew Suozzo <msuozzo@google.com>
2024-06-27 16:16:37 -0400

    Small refactor

--- internal/api/apiservice/rebuild.go
+++ internal/api/apiservice/rebuild.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"context"
 	"crypto"
-	"encoding/json"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 	"time"
@@ -123,7 +121,7 @@ type RebuildPackageDeps struct {
 type StrategyAndOrigin struct {
 	Strategy       rebuild.Strategy
 	ManualStrategy rebuild.Strategy
-	buildDefLoc    rebuild.Location
+	BuildDefLoc    rebuild.Location
 }
 
 func getStrategy(ctx context.Context, deps *RebuildPackageDeps, t rebuild.Target, fromRepo bool) (*StrategyAndOrigin, error) {
@@ -149,7 +147,7 @@ func getStrategy(ctx context.Context, deps *RebuildPackageDeps, t rebuild.Target
 			return nil, api.AsStatus(codes.Internal, errors.Wrap(err, "creating build definition repo reader"))
 		}
 		pth, _ := defs.Path(ctx, t)
-		strat.buildDefLoc = rebuild.Location{
+		strat.BuildDefLoc = rebuild.Location{
 			Repo: deps.BuildDefRepo.Repo,
 			Ref:  defs.Ref().String(),
 			Dir:  pth,
@@ -218,7 +216,7 @@ func buildAndAttest(ctx context.Context, deps *RebuildPackageDeps, mux rebuild.R
 		return api.AsStatus(codes.FailedPrecondition, errors.Wrap(err, "rebuild content mismatch"))
 	}
 	input := rebuild.Input{Target: t, Strategy: strat.ManualStrategy}
-	eqStmt, buildStmt, err := verifier.CreateAttestations(ctx, input, strat.Strategy, id, rb, up, metadata, strat.buildDefLoc)
+	eqStmt, buildStmt, err := verifier.CreateAttestations(ctx, input, strat.Strategy, id, rb, up, metadata, strat.BuildDefLoc)
 	if err != nil {
 		return api.AsStatus(codes.Internal, errors.Wrap(err, "creating attestations"))
 	}
@@ -228,28 +226,7 @@ func buildAndAttest(ctx context.Context, deps *RebuildPackageDeps, mux rebuild.R
 	return nil
 }
 
-func doRebuildPackage(ctx context.Context, req schema.RebuildPackageRequest, deps *RebuildPackageDeps, mux rebuild.RegistryMux, t rebuild.Target) (*StrategyAndOrigin, error) {
-	signer := verifier.InTotoEnvelopeSigner{EnvelopeSigner: deps.Signer}
-	a := verifier.Attestor{Store: deps.AttestationStore, Signer: signer, AllowOverwrite: deps.OverwriteAttestations}
-	if !deps.OverwriteAttestations {
-		if exists, err := a.BundleExists(ctx, t); err != nil {
-			return nil, api.AsStatus(codes.Internal, errors.Wrap(err, "checking existing bundle"))
-		} else if exists {
-			return nil, api.AsStatus(codes.AlreadyExists, errors.New("conflict with existing attestation bundle"))
-		}
-	}
-	strat, err := getStrategy(ctx, deps, t, req.StrategyFromRepo)
-	if err != nil {
-		return nil, api.AsStatus(codes.Internal, errors.Wrap(err, "getting strategy"))
-	}
-	err = buildAndAttest(ctx, deps, mux, a, t, *strat)
-	if err != nil {
-		return strat, api.AsStatus(codes.Internal, errors.Wrap(err, "executing rebuild"))
-	}
-	return strat, nil
-}
-
-func RebuildPackage(ctx context.Context, req schema.RebuildPackageRequest, deps *RebuildPackageDeps) (*schema.Verdict, error) {
+func rebuildPackage(ctx context.Context, req schema.RebuildPackageRequest, deps *RebuildPackageDeps) (*schema.Verdict, error) {
 	t := rebuild.Target{Ecosystem: req.Ecosystem, Package: req.Package, Version: req.Version}
 	ctx = context.WithValue(ctx, rebuild.HTTPBasicClientID, deps.HTTPClient)
 	regclient := httpinternal.NewCachedClient(deps.HTTPClient, &cache.CoalescingMemoryCache{})
@@ -261,37 +238,44 @@ func RebuildPackage(ctx context.Context, req schema.RebuildPackageRequest, deps 
 	if err := populateArtifact(ctx, &t, mux); err != nil {
 		return nil, api.AsStatus(codes.Internal, errors.Wrap(err, "selecting artifact"))
 	}
-	strat, rberr := doRebuildPackage(ctx, req, deps, mux, t)
-	v := schema.Verdict{
-		Target: t,
+	signer := verifier.InTotoEnvelopeSigner{EnvelopeSigner: deps.Signer}
+	a := verifier.Attestor{Store: deps.AttestationStore, Signer: signer, AllowOverwrite: deps.OverwriteAttestations}
+	v := schema.Verdict{Target: t}
+	if !deps.OverwriteAttestations {
+		if exists, err := a.BundleExists(ctx, t); err != nil {
+			return &v, api.AsStatus(codes.Internal, errors.Wrap(err, "checking existing bundle"))
+		} else if exists {
+			return &v, api.AsStatus(codes.AlreadyExists, errors.New("conflict with existing attestation bundle"))
+		}
 	}
-	if strat != nil {
-		v.StrategyOneof = schema.NewStrategyOneOf(strat.Strategy)
+	strat, err := getStrategy(ctx, deps, t, req.StrategyFromRepo)
+	if err != nil {
+		return &v, err
 	}
-	if rberr != nil {
-		v.Message = rberr.Error()
-	}
-	var rawStrategy string
-	if enc, err := json.Marshal(v.StrategyOneof); err != nil {
-		log.Printf("invalid strategy returned from smoketest: %v\n", err)
-	} else {
-		rawStrategy = string(enc)
-	}
+	v.StrategyOneof = schema.NewStrategyOneOf(strat.Strategy)
+	err = buildAndAttest(ctx, deps, mux, a, t, *strat)
+	return &v, err
+}
 
-	_, err := deps.FirestoreClient.Collection("ecosystem").Doc(string(v.Target.Ecosystem)).Collection("packages").Doc(sanitize(v.Target.Package)).Collection("versions").Doc(v.Target.Version).Collection("attempts").Doc(req.ID).Set(ctx, schema.SmoketestAttempt{
+func RebuildPackage(ctx context.Context, req schema.RebuildPackageRequest, deps *RebuildPackageDeps) (*schema.Verdict, error) {
+	v, err := rebuildPackage(ctx, req, deps)
+	if err != nil {
+		v.Message = err.Error()
+	}
+	_, err = deps.FirestoreClient.Collection("ecosystem").Doc(string(v.Target.Ecosystem)).Collection("packages").Doc(sanitize(v.Target.Package)).Collection("versions").Doc(v.Target.Version).Collection("attempts").Doc(req.ID).Set(ctx, schema.SmoketestAttempt{
 		Ecosystem:       string(v.Target.Ecosystem),
 		Package:         v.Target.Package,
 		Version:         v.Target.Version,
 		Artifact:        v.Target.Artifact,
 		Success:         v.Message == "",
 		Message:         v.Message,
-		Strategy:        rawStrategy,
+		Strategy:        v.StrategyOneof,
 		ExecutorVersion: os.Getenv("K_REVISION"),
 		RunID:           req.ID,
 		Created:         time.Now().UnixMilli(),
 	})
 	if err != nil {
-		return nil, api.AsStatus(codes.Internal, errors.Wrap(err, "storing results in firestore"))
+		return v, api.AsStatus(codes.Internal, errors.Wrap(err, "storing results in firestore"))
 	}
-	return &v, nil
+	return v, nil
 }
