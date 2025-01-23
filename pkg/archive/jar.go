552ba77ddc31295b9e5a5bcf53cc670b007ca661
Aman Sharma <mannu.poski10@gmail.com>
2025-01-23 14:58:12 +0100

    feat: add stabilizer to preserve order of values

--- pkg/archive/jar.go
+++ pkg/archive/jar.go
@@ -2,6 +2,7 @@ package archive
 
 import (
 	"bytes"
+	"sort"
 	"strings"
 )
 
@@ -69,5 +70,38 @@ var StableJARSignatureFiles = ZipEntryStabilizer{
 		}
 		// Zero out signature files
 		zf.SetContent([]byte{})
+	},
+}
+
+var StableJAROrderOfAttributeValues = ZipEntryStabilizer{
+	Name: "jar-attribute-value-order",
+	Func: func(zf *MutableZipFile) {
+		if !strings.HasSuffix(zf.Name, "META-INF/MANIFEST.MF") {
+			return
+		}
+		r, err := zf.Open()
+		if err != nil {
+			return
+		}
+		manifest, err := ParseManifest(r)
+		if err != nil {
+			return
+		}
+		for _, attr := range []string{
+			"Export-Package",
+		} {
+			value, _ := manifest.MainSection.Get(attr)
+			value = strings.ReplaceAll(value, "\r", "")
+			value = strings.ReplaceAll(value, "\n", "")
+			value = strings.ReplaceAll(value, " ", "")
+			commaSeparateValues := strings.Split(value, ",")
+			sort.Strings(commaSeparateValues)
+			manifest.MainSection.Set(attr, strings.Join(commaSeparateValues, ","))
+		}
+		buf := bytes.NewBuffer(nil)
+		if err := WriteManifest(buf, manifest); err != nil {
+			return
+		}
+		zf.SetContent(buf.Bytes())
 	},
 }
